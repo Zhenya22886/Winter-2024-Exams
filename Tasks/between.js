874158243849b0389a4b21e7a6ac214658e9ddaf
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 20:32:29 +0200

    Step 3 between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,25 +1,16 @@
 // Extract substring between prefix and suffix
 // Step 1 change name
 // Step 2 added const, let and use strict
+// Step 3 Improved readability
 
 'use strict'
 
-const getValuebetween  = (fullString, prefix, suffix) => {
- const index  = fullString.indexOf(prefix);
-  if (index  === -1) return '';
-  else {
-   let prefixLength  = index  + prefix.length;
-    fullString = fullString.substring(prefixLength );
-    if (suffix) {
-      index  = fullString.indexOf(suffix);
-      if (index  === -1) {
-        return '';
-      } else {
-        fullString = fullString.substring(0, index);
-      }
-    }
-  }
-  return fullString;
+
+const getValuebetween = (fullString, prefix, suffix) => {
+  const start = fullString.indexOf(prefix);
+  const end = fullString.indexOf(suffix);
+  if (start === -1 || end === -1) return '';
+  return fullString.substring(start + 1, end);
 };
 
 module.exports = getValuebetween ;
