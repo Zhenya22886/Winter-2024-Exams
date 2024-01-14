ebddbd9f30688c67db5ad488b94ef80ef8528b27
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 19:25:19 +0200

    Step 3 duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,15 +1,14 @@
 // Return an array without duplicates
 // Step 1 change name
 // Step 2 added const
+// Step 3 The else block can be omitted in both versions, as placing the return statement immediately after the if condition achieves the same outcome.
 
 const noDuplicatesArray  = (value, size) => {
-  if (size <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < size; i++) {
-      res[i] = value;
-    }
-    return res;
+  if (size <= 0) {
+    return [];
+  } else {
+    const uniqueArray = Array.from({ length: size }, () => value);
+    return uniqueArray;
   }
 };
 
