73000e8f746f92ae4194264e370360270f94df35
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 21:18:50 +0200

    Step 2 ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,13 +1,16 @@
 // Split string by the first occurrence of separator
 // Step 1 change name 
+// Step 2 added const, let and use strict
 
-parseIP  = (ipString ) => {
-  ipArray  = [];
+'use strict';
+
+const vparseIP  = (ipString ) => {
+ const ipArray  = [];
   if (ipString  === '') return;
   else {
-    arrOfIpElements  = ipString.split('.');
+   const arrOfIpElements  = ipString.split('.');
     if (arrOfIpElements.length != 4) return;
-    j = 0;
+   let j = 0;
     for (const ipElement  of arrOfIpElements ) {
       ipArray [j] = parseInt(ipElement );
       if (isNaN(ipArray [j])) return;
