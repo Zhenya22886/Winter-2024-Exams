ac3f20953107a52ec716b24312b48b0fa05cd22b
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-15 00:41:49 +0200

    Update password.js

--- Tasks/password.js
+++ Tasks/password.js
@@ -2,6 +2,7 @@
 // Step 1 change name\
 // Step 2 added const and let
 // Step 3 Improved readability 
+'use strict';
 
 const generatePassword = (alphabet, length) => {
   const alphabetLength  = alphabet.length;
