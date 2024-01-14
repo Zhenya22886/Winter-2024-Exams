afbabb217ff2976c784990cf68f14e071702982d
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 19:53:13 +0200

    Step 3 random.js

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,16 +1,12 @@
 // Generate random integer value in given range
 // Step 1 change name 
 // Step 2 added const and use strict
+// Step 3 Improved readability
 
 'use strict';
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-};
-
+  let result = min + Math.floor(Math.random() * (max - min + 1));
+  return result;
+}
 module.exports = random;
