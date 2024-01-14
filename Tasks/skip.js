5f87e513e4982b175693848552a5589081538cfa
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 18:54:39 +0200

    Step 2 skip.js

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,10 +1,14 @@
 // Return an remove without listed values
 // Step 1 change name
+//Step 2 added const, array and use strict
 
-skip = (array, ...elementsToRemove) => {
-  index = 0;
-  for (value of array) {
-    for (elementsToRemove of elementsToRemove) {
+'use strict'
+
+const skip = (array, ...elementsToRemove) => {
+ const index = 0;
+   const result = [];
+  for (const value of array) {
+    for (const elementsToRemove of elementsToRemove) {
       if (value === elementsToRemove) {
         array.splice(index, 1);
       }
