ebf2235dca2fbd9894b29ef77b127bfdb32b486d
Женя <147931012+Zhenya22886@users.noreply.github.com>
2024-01-14 20:15:02 +0200

    Step 3 day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,13 +1,14 @@
 // Get day number
 // Step 1 change name 
 // Step 2 added const and use strict 
-'use strict'
+// Step 3 Improved readability
+'use strict';
 
 let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const daysOrder  = (dayString) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
+  
+  for (let i = 0; i < days.length; i++) {
     if (dayString.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
